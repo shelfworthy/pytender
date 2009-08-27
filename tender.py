d637c39f5e465159972b36b3578e13bf7554df01
chrisdrackett <chris@chrisdrackett.com>
2009-08-27 16:30:43 -0500

    adding comments to discussions.

--- tender.py
+++ tender.py
@@ -35,7 +35,7 @@ class TenderClient(object):
         self.href = self.values.href
     
     def profile(self):
-        return TenderUser(self.values.profile_href, self)
+        return TenderUser(self, self.values.profile_href)
 
     def discussions(self, page=None, state=None, category=None, user_email=None):
         url = build_url(self.values.discussions_href)
@@ -76,7 +76,7 @@ class TenderClient(object):
         return ResponseDict(simplejson.loads(response))
 
 class TenderUser(object):
-    def __init__(self, user_href, client):
+    def __init__(self, client, user_href):
         self.client = client
         
         self.raw_data = self.client.__get__(user_href)
@@ -114,7 +114,78 @@ class TenderUser(object):
         pass
 
 class TenderDiscussion(object):
-    pass
+    def __init__(self, client, discussion_href=None):
+        self.client = client
+        self.raw_data = self.client.__get__(user_href)
+        
+        if discussion_href:
+            self.is_complete = False
+        else:
+            self.is_complete = True
+            
+
+    def fetch_more(self):
+        ''' if this discussion is from a list, this function 
+        will fetch things like the comments that are only available when
+        getting a discussion by itself'''
+        self.raw_data = self.client.__get__(self.raw_data.href)
+        self.is_complete = True
+
+    @property
+    def number(self):
+        return self.raw_data.number
+
+    @property
+    def title(self):
+        return self.raw_data.title
+
+    @property
+    def user(self):
+        return TenderUser(self.client, self.raw_data.user_href)
+
+    @property
+    def public(self):
+        return self.raw_data.public
+
+    def comments(self):
+        if not self.is_complete:
+            self.fetch_more()
+        comments = []
+        for raw_comment in self.raw_data.comments:
+            comments.append(TenderComment(self.client, raw_comment))
+        return comments
+
+class TenderComment(object):
+    def __init__(self, client, raw_comment):
+        self.client = client
+        self.raw_data = raw_comment
+
+    @property
+    def number(self):
+        return self.raw_data.number
+
+    def body(self):
+        return self.raw_data.body
+    @property
+    def via(self):
+        return self.raw_data.via
+
+    @property
+    def user(self):
+        return TenderUser(self.client, self.raw_data.user_href)
+
+    @property
+    def user_is_supporter(self):
+        return self.raw_data.user_is_supporter
+
+    @property
+    def resolution(self):
+        return self.raw_data.resolution
+
+    @property
+    def created_at(self):
+        return date_from_string(self.raw_data.created_at)
+
 class TenderCategory(object):
     pass
 class TenderQueue(object):
